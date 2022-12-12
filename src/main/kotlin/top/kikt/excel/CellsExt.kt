9d82d8576621bee452f9b81f965f12ee16820b58
caijinglong-b-imac <cjl_spy@163.com>
2022-12-12 13:42:01 +0800

    :sparkles: Extracted some cell codes

--- src/main/kotlin/top/kikt/excel/CellsExt.kt
+++ src/main/kotlin/top/kikt/excel/CellsExt.kt
@@ -1,11 +1,11 @@
 package top.kikt.excel
 
+import org.apache.poi.hssf.usermodel.HSSFRichTextString
 import org.apache.poi.hssf.usermodel.HSSFWorkbook
 import org.apache.poi.hssf.util.HSSFColor
-import org.apache.poi.ss.usermodel.Cell
-import org.apache.poi.ss.usermodel.Color
-import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.ss.usermodel.*
 import org.apache.poi.xssf.usermodel.XSSFColor
+import org.apache.poi.xssf.usermodel.XSSFRichTextString
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
 
 /**
@@ -51,6 +51,69 @@ fun Cell.copyCellFont(other: Cell) {
 }
 
 /**
+ * Copy cell style to target cell
+ */
+fun Cell.copyStyle(other: Cell) {
+    val src = this.cellStyle
+    val target = other.workbook.createCellStyle()
+
+    if (src?.javaClass == target?.javaClass) {
+        target.cloneStyleFrom(src)
+        return
+    }
+
+    val otherStyle = other.cellStyle
+    // ignore font , because font is alone method
+
+    // copy border
+    with(this.cellStyle) {
+        otherStyle.borderBottom = this.borderBottom
+        otherStyle.borderLeft = this.borderLeft
+        otherStyle.borderRight = this.borderRight
+        otherStyle.borderTop = this.borderTop
+
+        // copy border color
+        otherStyle.bottomBorderColor = this.bottomBorderColor
+        otherStyle.leftBorderColor = this.leftBorderColor
+        otherStyle.rightBorderColor = this.rightBorderColor
+        otherStyle.topBorderColor = this.topBorderColor
+
+        // copy fill
+        this.fillBackgroundColorColor?.toColor(other.workbook)?.toColorIndex(other.workbook)
+            ?.let {
+                otherStyle.fillBackgroundColor = it
+            }
+        this.fillForegroundColorColor?.toColor(other.workbook)?.toColorIndex(other.workbook)
+            ?.let {
+                otherStyle.fillForegroundColor = it
+            }
+
+        // copy fill pattern
+        otherStyle.fillPattern = this.fillPattern
+
+        // copy alignment
+        otherStyle.alignment = this.alignment
+        otherStyle.verticalAlignment = this.verticalAlignment
+
+        // copy wrap text
+        otherStyle.wrapText = this.wrapText
+
+        // copy date format
+        otherStyle.dataFormat
+    }
+}
+
+private fun Color.toColorIndex(dst: Workbook): Short? {
+    val color = toColor(dst)
+    if (color is XSSFColor) {
+        return color.indexed
+    } else if (color is HSSFColor) {
+        return color.index
+    }
+    return null
+}
+
+/**
  * The color to another workbook.
  */
 fun Color.toColor(dstWorkbook: Workbook): Color? {
@@ -93,4 +156,87 @@ fun Color.getColorRgb(): ByteArray {
     }
 
     return rgbArray.toByteArray()
+}
+
+/**
+ * Copy cell value to target cell.
+ */
+fun Cell.copyCellValue(other: Cell) {
+    try {
+        when (cellType) {
+            CellType.BLANK -> other.setBlank()
+            CellType.BOOLEAN -> other.setCellValue(this.booleanCellValue)
+            CellType.ERROR -> other.setCellErrorValue(this.errorCellValue)
+            CellType.FORMULA -> other.cellFormula = this.cellFormula
+            CellType.NUMERIC -> other.setCellValue(this.numericCellValue)
+            CellType.STRING -> this.copyStringValueTo(other)
+            else -> {
+            }
+        }
+    } catch (e: Exception) {
+        other.setCellValue(stringValue())
+    }
+}
+
+/**
+ * The cell is same class with other cell.
+ */
+fun Cell.isSameClass(other: Cell): Boolean {
+    return this.javaClass == other.javaClass
+}
+
+private fun Cell.copyStringValueTo(other: Cell) {
+    if (cellType != CellType.STRING) {
+        return
+    }
+    if (isSameClass(other)) {
+        other.setCellValue(this.richStringCellValue)
+        return
+    }
+
+    // if workbook is not same type, use string value
+    val targetValue: RichTextString =
+        when (val richStringCellValue = this.richStringCellValue) {
+            is XSSFRichTextString -> {
+                convertToHSSFRichTextString(richStringCellValue, other.workbook)
+            }
+
+            is HSSFRichTextString -> {
+                convertToXSSFRichTextString(richStringCellValue, other.workbook)
+            }
+
+            else -> {
+                null
+            }
+        } ?: return
+
+    other.setCellValue(targetValue)
+}
+
+private fun Cell.convertToHSSFRichTextString(src: XSSFRichTextString, targetWorkbook: Workbook): HSSFRichTextString {
+    val hssfRichTextString = HSSFRichTextString(src.string)
+
+    for (formatIndex in 0 until src.numFormattingRuns()) {
+        val srcFont = src.getFontOfFormattingRun(formatIndex) ?: continue
+        val formatStart = src.getIndexOfFormattingRun(formatIndex)
+        val formatLength = src.getLengthOfFormattingRun(formatIndex)
+        val targetFont = srcFont.copy(workbook, targetWorkbook)
+
+        hssfRichTextString.applyFont(formatStart, formatStart + formatLength, targetFont)
+    }
+
+    return hssfRichTextString
+}
+
+private fun Cell.convertToXSSFRichTextString(src: HSSFRichTextString, targetWorkbook: Workbook): XSSFRichTextString {
+    val xssfRichTextString = XSSFRichTextString(src.string)
+
+    for (i in 0 until src.string.count()) {
+        val fontIndex = src.getFontAtIndex(i)
+        val font = workbook.getFontAt(fontIndex.toInt())
+        val targetFont = font.copy(workbook, targetWorkbook)
+        xssfRichTextString.applyFont(i, i + 1, targetFont)
+    }
+
+    return xssfRichTextString
 }
